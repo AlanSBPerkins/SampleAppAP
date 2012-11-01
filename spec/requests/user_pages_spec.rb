b8c52a4ab8339833181b8e7a116f643008b21e30
AlanPerkins <asbperkins@gmail.com>
2012-11-02 01:05:31 +1100

    complete handling deletions - 9.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -11,8 +11,8 @@ describe "User Pages" do
 			visit users_path
 		end
 
-		it { should have_selector('h1', 	text: 'All users') }
 		it { should have_selector('title', 	text: 'All users') }
+		it { should have_selector('h1', 	text: 'All users') }
 
 		describe "pagination" do
 
@@ -27,6 +27,25 @@ describe "User Pages" do
 				end
 			end
 		end
+
+		describe "delete links" do
+			it { should_not have_link('delete') }
+
+			describe "as an admin user" do
+				let(:admin) { FactoryGirl.create(:admin) } 
+				before do
+					valid_signin admin
+					visit users_path
+				end
+
+				it { should have_link('delete', href: user_path(User.first)) }
+				it "should be able to delete another user" do
+					expect { click_link('delete') }.to change(User, :count).by(-1)
+				end
+				it { should_not have_link('delete', href: user_path(admin)) }
+			end
+		end
+
 	end
 
 	describe "signup page" do
