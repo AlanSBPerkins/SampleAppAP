a8c433840f7ab15fcc6a70df4166b2776ac2d967
AlanPerkins <asbperkins@gmail.com>
2012-11-01 20:52:03 +1100

    complete 9.3 - pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
-FactoryGirl.define  do
-	factory :user do
-		name			"Alan Perkins"
-		email			"asbperkins@gmail.com"
-		password	"foobar"
-		password_confirmation 	"foobar"
-	end
+FactoryGirl.define do
+  factory :user do
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+  end
 end
