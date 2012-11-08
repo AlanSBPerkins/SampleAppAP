a81d87d0ac05ae4c62d28678bf0dfae11c8db143
AlanPerkins <asbperkins@gmail.com>
2012-11-08 14:56:43 +1100

    after delete microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   before_filter :correct_user,   only: [:edit, :update]
   before_filter :admin_user,     only: :destroy
 
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
 	def show
 		@user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
@@ -32,17 +37,13 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated"
       sign_in @user
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
     end
   end  
-
-  def index
-    @users = User.paginate(page: params[:page])
-  end
 
   def destroy
     user = User.find(params[:id])
@@ -57,19 +58,13 @@ class UsersController < ApplicationController
   end
 
 private
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in." 
-    end  
-  end  
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
+    redirect_to root_path unless current_user?(@user)
   end
 
   def admin_user
-    redirect_to(root_path) unless current_user.admin?
+    redirect_to root_path unless current_user.admin?
   end 
 end
